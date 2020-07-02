95f518619733eeea5e3e7091c741ba7587316f01
MuninHugin <40742567+MuninHugin@users.noreply.github.com>
2020-07-02 14:43:00 +0300

    Первичная настройка вебпак

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-// import $ from 'jquery';
-import avg from './some.js';
-
-console.log(avg(3,4,5,11));
-// $('.div').html('Some text 1211');
+import './js/common'
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
-let path = require('path');
+const path = require('path')
 
-let conf = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, './dist/'),
-		filename: 'main.js',
-		publicPath: 'dist/'
-	},
-	devServer: {
-		overlay: true
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				// exclude: '/node_modules/'
-			}
-		]
-	}
-};
-
-module.exports = conf;
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules/'
+        }]
+    },
+    devServer: {
+        overlay: true
+    }
+}
