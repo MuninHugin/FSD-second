d7a7549b62a7f081d0f30f856915678499eb263b
MuninHugin <40742567+MuninHugin@users.noreply.github.com>
2020-07-03 18:05:30 +0300

    обработка картинок и html. Webpack-merge. Обработка статических файлов

---
+++ build/webpack.build.config.js
@@ -0,0 +1,13 @@
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.config')
+const { resolve } = require('path')
+const { rejects } = require('assert')
+
+const buildWebpackConfig = merge(baseWebpackConfig, {
+    mode: 'production',
+    plugins: []
+})
+
+module.exports = new Promise((resolve, reject) => {
+    resolve(buildWebpackConfig)
+})
